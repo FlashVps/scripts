40a3690f1d78b26e4e206a36a1dec0581f4980b3
FlashVps <vanpotter278@gmail.com>
2017-04-09 13:21:10 -0300

    Update vpsmanager.sh

--- vpsmanager.sh
+++ vpsmanager.sh
@@ -93,7 +93,7 @@ elif [[ "$resposta" = '7' ]]; then
         rm gerenciadoropen.sh
 elif [[ "$resposta" = '8' ]]; then
         rm mudarvalidade.sh
-        wget https://raw.githubusercontent.com/FlashVps/scripts/master/mudardata.sh
+        wget https://raw.githubusercontent.com/FlashVps/scripts/master/mudarvalidade.sh
         chmod +x mudarvalidade.sh
         clear
         ./mudarvalidade.sh
